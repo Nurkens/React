d1fd1a7e16e9bb5a7e057c9b73edbbd19ad2c8a1
Nurken <lider.2005.1@gmail.com>
2026-06-26 18:24:03 +0500

    Move pages to React

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 function App() {
  
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<CheckoutPage />}></Route>
+        <Route path="orders" element={<OrdersPage />}></Route>
+        <Route path="tracking" element={<TrackingPage />}></Route>  
       </Routes>
       
     </>
